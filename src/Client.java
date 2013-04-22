4f342996d2513d81fac7d15e7d387fc10a6c2cc7
Nate Jang <nate.jang@sk.com>
2013-04-22 19:46:53 +0900

    April 22, homework #1/2

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+
+public class Client {
+
+	public void biz() {
+		Log l = new DefaultLog();
+		l.write("aaa");
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.biz();
+	}
+}
